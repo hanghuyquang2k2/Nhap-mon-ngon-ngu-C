f5a2175d535d092522fce735950365564a2cba58
hanghuyquang2k2 <72533418+hanghuyquang2k2@users.noreply.github.com>
2020-12-03 08:40:22 +0700

    Create HINHTRON.C

---
+++ Bài tập 1/HINHTRON.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+//khai bao bien DT, VC, và R
+  double DT, VC, R;
+  //khai bao bang pi
+const double PI= 3.14;  
+// gan gia tri DT, VC
+DT = R * R * PI;
+VC = 2 * R * PI;
+printf("VC hinh tron la: %0.1f/n", CV);
+printf("DT hinh tron la:%0.1f/n", DT);
+return 0;
+}
